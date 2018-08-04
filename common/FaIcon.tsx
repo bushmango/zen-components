d9369ea506bf5e93e8b600da5ef010b74ff08a9a
Unknown <bushmango@gmail.com>
2018-08-03 20:26:20 -0500

    udpate faicon

--- common/FaIcon.tsx
+++ common/FaIcon.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const FaIcon = (props: { icon: string; size?: string; prefix?: string }) => {
+const FaIcon = (props: {
+  icon: string
+  size?: string
+  prefix?: string
+  color?: string
+  style?: any
+}) => {
   let className = (props.prefix || 'fas') + ' fa-' + props.icon
   if (props.size) {
     className += ' fa-' + props.size
   }
-  return <span><i className={className} /></span>
+  return (
+    <span>
+      <i
+        className={className}
+        style={props.style ? props.style : { color: props.color }}
+      />
+    </span>
+  )
 }
 
 export { FaIcon }
